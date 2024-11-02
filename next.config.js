c7652bcadf9e023bd5ff3de28570365d22a24e1f
Manish Chhetri <chhetridon123@gmail.com>
2024-11-02 16:35:30 +0545

    added "Nepali Caption blog"

--- next.config.js
+++ next.config.js
@@ -10,6 +10,17 @@ const nextConfig = {
       },
     ];
   },
+
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        punycode: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
