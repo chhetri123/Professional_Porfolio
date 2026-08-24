534686c9580b4086c12ab03ab8a0212d3afc0d9f
Manish Chhetri <65161301+chhetri123@users.noreply.github.com>
2026-08-24 23:00:16 +0545

    fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,22 @@
 module.exports = {
   root: true,
+
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
+
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react/recommended",
     "plugin:react-hooks/recommended",
-    "plugin:@next/next/recommended",
     "next/core-web-vitals",
   ],
+
   parser: "@typescript-eslint/parser",
+
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -21,12 +24,15 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
+
   plugins: ["@typescript-eslint", "react"],
+
   settings: {
     react: {
       version: "detect",
     },
   },
+
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
